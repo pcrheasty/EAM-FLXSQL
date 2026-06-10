86c6058311b96e1dbb39e6b987ae44e33ad1fb00
pcrheasty <ross.heasty@piercecountywa.gov>
2026-06-10 11:40:30 -0700

    Update Update IT Asset based on Disposal State.sql

--- Update IT Asset based on Disposal State.sql
+++ Update IT Asset based on Disposal State.sql
@@ -1,26 +1,33 @@
 DECLARE
-    DRCode NVARCHAR2(30);
-    DRState NVARCHAR2(30);
+  vSRCode      VARCHAR2(30);
+  vSRType      VARCHAR2(30);
+  vSRDispType  VARCHAR2(30);
+  vPRCode      VARCHAR2(30);
+  vPRProperty  VARCHAR2(30);
+  vPRValue     VARCHAR2(80);
 
 BEGIN
-    SELECT ctr_code,
-           (SELECT prv_value
-              FROM r5propertyvalues
-             WHERE prv_property = 'IT-DISP'
-               AND prv_code = (r5contactrecords.ctr_code || '#IT'))
-      INTO DRCode, DRState
-      FROM r5contactrecords
-     WHERE ROWID = :ROWID;
-
-    IF (DRState = 'Picked Up - PCL Disposal') THEN
+  SELECT prv_code, prv_property, prv_value
+  INTO vPRCode, vPRProperty, vPRValue
+  FROM r5propertyvalues
+  WHERE ROWID = :ROWID AND prv_property = 'IT-DISP';
+  
+  vPRCode := TRIM(REPLACE(vPRCode,'#IT',''));
+  
+  SELECT ctr_code
+  INTO vSRCode
+  FROM r5contactrecords 
+  WHERE ctr_code =  vPRCode;
+ 
+	IF vPRValue = 'Picked Up - PCL Disposal' AND vSRType = 'ITDF' AND vSRDispType = 'Disposal' 
+  THEN
         UPDATE r5objects o
            SET o.OBJ_UDFCHAR35 = 'PICKEDUP',
                o.OBJ_UDFCHAR38 = 'DISPOSE'
-         WHERE o.obj_udfchar24 = 'CPU'
-           AND o.OBJ_CODE IN (
+         WHERE o.obj_udfchar24 = 'CPU' AND o.OBJ_CODE IN (
                SELECT d.DESP_ASSETID
                  FROM u5itamdespequip d
-                WHERE d.DESP_SRCODE = DRCode
+                WHERE d.DESP_SRCODE = vSRCode
            );
     END IF;
 
